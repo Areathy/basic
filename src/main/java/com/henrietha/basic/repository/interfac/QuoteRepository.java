9aa27e052af099a3d9697c2ecf7672a0f46dd6a9
Henrietha <areathy33@gmail.com>
2021-11-25 23:04:07 +0100

    Add repository interface

--- src/main/java/com/henrietha/basic/repository/interfac/QuoteRepository.java
+++ src/main/java/com/henrietha/basic/repository/interfac/QuoteRepository.java
@@ -5,4 +5,10 @@ import com.henrietha.basic.entities.Quote;
 import java.util.List;
 import java.util.Optional;
 
-public interface QuoteRepository {}
+public interface QuoteRepository {
+    String saveQuote(Quote quote) throws Exception;
+    String updateQuote(Quote quote) throws Exception;
+    Optional<List<Quote>> getAllQuote(Quote quote) throws Exception;
+    Optional<Quote> getQuoteById(String Quote) throws Exception;
+    Optional<Quote> deleteQuote(long id) throws Exception;
+}
