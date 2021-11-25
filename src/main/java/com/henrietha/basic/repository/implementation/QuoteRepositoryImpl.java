d818ab901dd37077eaec1d1f2e151bd069f119fd
Henrietha <areathy33@gmail.com>
2021-11-25 23:05:33 +0100

    Implement's repository interface

--- src/main/java/com/henrietha/basic/repository/implementation/QuoteRepositoryImpl.java
+++ src/main/java/com/henrietha/basic/repository/implementation/QuoteRepositoryImpl.java
@@ -1,4 +1,34 @@
 package com.henrietha.basic.repository.implementation;
 
-public class QuoteRepositoryImpl {
+import com.henrietha.basic.entities.Quote;
+import com.henrietha.basic.repository.interfac.QuoteRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class QuoteRepositoryImpl implements QuoteRepository {
+    @Override
+    public String saveQuote(Quote quote) throws Exception {
+        return null;
+    }
+
+    @Override
+    public String updateQuote(Quote quote) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Optional<List<Quote>> getAllQuote(Quote quote) throws Exception {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Quote> getQuoteById(String Quote) throws Exception {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Quote> deleteQuote(long id) throws Exception {
+        return Optional.empty();
+    }
 }
