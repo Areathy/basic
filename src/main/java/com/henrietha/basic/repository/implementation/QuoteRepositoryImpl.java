8a9fa1e6e47e2eca830552c119b9e2bbcb8f5638
Henrietha <areathy33@gmail.com>
2021-11-25 23:22:51 +0100

    Update QuoteRepositoryImpl.java

--- src/main/java/com/henrietha/basic/repository/implementation/QuoteRepositoryImpl.java
+++ src/main/java/com/henrietha/basic/repository/implementation/QuoteRepositoryImpl.java
@@ -2,11 +2,16 @@ package com.henrietha.basic.repository.implementation;
 
 import com.henrietha.basic.entities.Quote;
 import com.henrietha.basic.repository.interfac.QuoteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
 
 public class QuoteRepositoryImpl implements QuoteRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(QuoteRepositoryImpl.class);
+
     @Override
     public String saveQuote(Quote quote) throws Exception {
         return null;
